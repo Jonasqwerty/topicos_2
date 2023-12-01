3979ba7795c78378b67b4ec80fd5a32dd472e3a7
Jonasqwerty <106714952+Jonasqwerty@users.noreply.github.com>
2023-12-01 14:12:39 -0300

    crud_mongodb.py

--- topicos_2/crud_mongodb.py
+++ topicos_2/crud_mongodb.py
@@ -5,27 +5,22 @@ client = MongoClient("mongodb+srv://aluno:aluno@cluster0.e24zln7.mongodb.net/?re
 db = client["jonasdb"]
 collection = db["produtodb"]
 
-# Função para criar um usuário
 def insert(data):
     result = collection.insert_one(data)
     return str(result.inserted_id)
 
-# Função para obter todos os usuários
 def get_all_produtodb():
     produtodb = collection.find()
     return [user for user in produtodb]
 
-# Função para obter um usuário pelo ID
 def get_user_by_id(_id):
     user = collection.find_one({"_id": ObjectId(_id)})
     return user
 
-# Função para atualizar um usuário pelo ID
 def update_user(_id, new_data):
     result = collection.update_one({"_id": ObjectId(_id)}, {"$set": new_data})
     return result.modified_count
 
-# Função para excluir um usuário pelo ID
 def delete_user(_id):
     result = collection.delete_one({"_id": ObjectId(_id)})
     return result.deleted_count
@@ -36,39 +31,35 @@ if __name__ == "__main__":
                  "validade" : "02/12/2023",
                  "quantidade" : 3}
 
-    # Create
     created__id = insert(data)
 
-    # Read all
     all_produtodb = get_all_produtodb()
     print("Produtos:")
     for user in all_produtodb:
         print(user)
-
-   
 
     # Update
     _id_to_update = created__id
     update_data = {"quantidade" : 6}
     updated_count = update_user(_id_to_update, update_data)
 
-    # Read all after update
     all_produtodb_after_update = get_all_produtodb()
     print("Produtos depois do UPDATE:")
     for user in all_produtodb_after_update:
         print(user)
 
+    
     # Delete
     _id_delete = created__id
     deleted_count = delete_user(_id_delete)
 
-    # Read all after delete
     all_produtodb_after_delete = get_all_produtodb()
     print("Produtos depois do DELETE:")
     for user in all_produtodb_after_delete:
         print(user)
 
 
-pip install pymongo
+pip install pymongo==3.7.2
+pip install dnspython
 
 python crud_mongodb.py
